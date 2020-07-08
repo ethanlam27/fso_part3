171438c1dc48e43abc21e8b2ac2ae11ecc7fd958
unknown <lam.ethan27@gmail.com>
2020-07-08 10:38:20 -0400

    build fix

---
+++ build/precache-manifest.1779a9ec810f580601f8563462ad0a50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2eb3bba5974c67f7a67eabfafcad7be",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1bc8cd2cbd95e8078c88",
+    "url": "/static/js/2.88aa3a0c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.88aa3a0c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8907b76d09ef27ae4b29",
+    "url": "/static/js/main.261d4a2c.chunk.js"
+  },
+  {
+    "revision": "40d77226df6c86a86844",
+    "url": "/static/js/runtime-main.18dc0f42.js"
+  }
+]);
