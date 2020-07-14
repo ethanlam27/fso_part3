370008b187dbe3728e2967361930fe0e1f0c5d7c
unknown <lam.ethan27@gmail.com>
2020-07-14 11:36:49 -0400

    eslint

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const db = require('./models/db')
-const { response } = require('express')
 
 
 // -- MIDDLEWARE --
@@ -12,7 +11,7 @@ const { response } = require('express')
 app.use(express.json())
 
 morgan.token('postbody', (req, res) => {
-    return JSON.stringify(req.body)
+  return JSON.stringify(req.body)
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postbody'))
@@ -21,112 +20,112 @@ app.use(express.static('build'))
 // -- ROUTES --
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/info', (req, res) => {
-    db.connectToDB()
-    db.PrsonModel.find({})
-        .then(result => {
-            const numEntries = result.length
-            res.send(`<p>Phonebook has info for ${numEntries} people</p><p>${new Date()}</p>`)
-            db.closeDB()
-        })
-        .catch(error => next(error))
+app.get('/info', (req, res, next) => {
+  db.connectToDB()
+  db.PrsonModel.find({})
+    .then(result => {
+      const numEntries = result.length
+      res.send(`<p>Phonebook has info for ${numEntries} people</p><p>${new Date()}</p>`)
+      db.closeDB()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons', (req, res, next) => {
-    db.connectToDB()
-    db.PrsonModel.find({})
-        .then(result => {
-            res.json(result)
-            db.closeDB()
-        })
-        .catch(error => next(error))
+  db.connectToDB()
+  db.PrsonModel.find({})
+    .then(result => {
+      res.json(result)
+      db.closeDB()
+    })
+    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    // string
-    const desiredID = req.params.id
+  // string
+  const desiredID = req.params.id
 
-    db.connectToDB()
-    db.PrsonModel.findById(desiredID)
-        .then(result => {
-            res.json(result)
-            db.closeDB()
-        })
-        .catch(error => next(error))
+  db.connectToDB()
+  db.PrsonModel.findById(desiredID)
+    .then(result => {
+      res.json(result)
+      db.closeDB()
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
+  const id = req.params.id
 
-    db.connectToDB()
-    db.PrsonModel.findByIdAndRemove(id)
-        .then(result => {
-            res.status(204).end()
-            db.closeDB()
-        })
-        .catch(error => next(error))
+  db.connectToDB()
+  db.PrsonModel.findByIdAndRemove(id)
+    .then(result => {
+      res.status(204).end()
+      db.closeDB()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res, next) => {
-    const note = req.body
+  const note = req.body
 
-    if (!note.name || !note.number) {
-        res.status(400).json({
-            error: 'name and number must exist'
-        })
-    } else {
-        const prsonToAdd = new db.PrsonModel({
-            name: note.name,
-            number: note.number
-        })
+  if (!note.name || !note.number) {
+    res.status(400).json({
+      error: 'name and number must exist'
+    })
+  } else {
+    const prsonToAdd = new db.PrsonModel({
+      name: note.name,
+      number: note.number
+    })
 
-        db.connectToDB()
-        prsonToAdd.save()
-            .then(savedPrson => {
-                res.json(savedPrson)
-                db.closeDB()
-            })
-            .catch(error => next(error))
-    }
+    db.connectToDB()
+    prsonToAdd.save()
+      .then(savedPrson => {
+        res.json(savedPrson)
+        db.closeDB()
+      })
+      .catch(error => next(error))
+  }
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const id = req.params.id
-    const person = 
+  const id = req.params.id
+  const person =
         {
-            name: req.body.name,
-            number: req.body.number
+          name: req.body.name,
+          number: req.body.number
         }
 
-    db.connectToDB()
-    db.PrsonModel.findOneAndUpdate({_id: id}, person, {new: true})
-        .then(updatedPerson => {
-            console.log(updatedPerson)
-            res.json(updatedPerson)
-            db.closeDB()
-        })
-        .catch(error => next(error))
+  db.connectToDB()
+  db.PrsonModel.findOneAndUpdate({ _id: id }, person, { new: true })
+    .then(updatedPerson => {
+      console.log(updatedPerson)
+      res.json(updatedPerson)
+      db.closeDB()
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
-    console.error('error message: ', error.message)
-    
-    if (error.name === 'ValidationError') {
-        return response
-            .status(400)
-            .json({error: error.message})
-    }
+  console.error('error message: ', error.message)
 
-    next(error)
+  if (error.name === 'ValidationError') {
+    return response
+      .status(400)
+      .json({ error: error.message })
   }
-  
+
+  next(error)
+}
+
 app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
 
--- models/db.js
+++ models/db.js
@@ -2,52 +2,52 @@ const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.set('useFindAndModify', false)
-mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true)
 
 
 const prsonSchema = new mongoose.Schema({
-    name: 
+  name:
     {
-        type: String,
-        minlength: 3,
-        unique: true,
-        required: true
+      type: String,
+      minlength: 3,
+      unique: true,
+      required: true
     },
 
-    number: 
+  number:
     {
-        type: String,
-        minlength: 8,
-        required: true
+      type: String,
+      minlength: 8,
+      required: true
     }
 })
 // Apply the uniqueValidator plugin to userSchema.
-prsonSchema.plugin(uniqueValidator);
+prsonSchema.plugin(uniqueValidator)
 
 prsonSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 const connectToDB = () => {
-    const url = process.env.MONGO_URL
-    console.log('connecting to ', url)
+  const url = process.env.MONGO_URL
+  console.log('connecting to ', url)
 
-    mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(result => {
-            console.log('connected to MongoDB')
-        })
-        .catch( error => {
-            console.log('error connecting to MongoDB: ', error.message)
-        })
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(result => {
+      console.log('connected to MongoDB')
+    })
+    .catch( error => {
+      console.log('error connecting to MongoDB: ', error.message)
+    })
 }
 
 const closeDB = () => {
-    mongoose.connection.close()
-    console.log('connection to MongoDB closed')
+  mongoose.connection.close()
+  console.log('connection to MongoDB closed')
 }
 
 /*
